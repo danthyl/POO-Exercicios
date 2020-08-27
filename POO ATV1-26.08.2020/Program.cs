f03097a967331ff94235c7056e6ee499b49ee8bb
StephNascimento <69490116+StephNascimento@users.noreply.github.com>
2020-08-26 22:55:33 -0300

    Add files via upload
    Exercícios da aula do dia 26/08, 1° mostra o mais velho e 2° mostra média salarial

---
+++ POO ATV1-26.08.2020/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace POO_ATV1_26._08._2020
+{
+    class Pessoas
+    {
+        public string p1, p2; //pessoas
+        public int idadep1, idadep2; //idade das pessoas
+    }
+
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+            Pessoas pessoa1, pessoa2;
+
+            pessoa1 = new Pessoas();
+            pessoa2 = new Pessoas();
+
+            Console.WriteLine("Escreva seu nome e idade: ");
+            pessoa1.p1 = Console.ReadLine();
+            pessoa1.idadep1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escreva seu nome e idade: ");
+            pessoa2.p2 = Console.ReadLine();
+            pessoa2.idadep2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados 1° Pessoa: ");
+            Console.WriteLine("Nome: " + pessoa1.p1);
+            Console.WriteLine("Idade: " + pessoa1.idadep1);
+            Console.WriteLine("Dados 2° Pessoa: ");
+            Console.WriteLine("Nome: " + pessoa2.p2);
+            Console.WriteLine("Idade: " + pessoa2.idadep2);
+
+            if(pessoa1.idadep1 > pessoa2.idadep2)
+            {
+                Console.WriteLine("Pessoa mais velha: " + pessoa1.p1); // tá rodando só o segundo
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + pessoa2.p2);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ POO ATV2-26.08.2020/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace POO_ATV2_26._08._2020
+{
+    class Funcionario
+    {
+        public string nome;
+        public double salario;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario A, B;
+            A = new Funcionario();
+            B = new Funcionario();
+
+            Console.WriteLine("Escreva seu nome e salário");
+            A.nome = Console.ReadLine();
+            A.salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escreva seu nome e salário");
+            B.nome = Console.ReadLine();
+            B.salario = double.Parse(Console.ReadLine());
+
+            double m = (A.salario + B.salario) / 2;
+
+            Console.WriteLine("1° Funcionário: ");
+            Console.WriteLine("Nome: " + A.nome);
+            Console.WriteLine("Salário: " + A.salario);
+            Console.WriteLine("2° Funcionário: ");
+            Console.WriteLine("Nome: " + B.nome);
+            Console.WriteLine("Salário: " + B.salario);
+            Console.WriteLine("Salário médio: " + m);
+            Console.ReadKey();
+        }
+    }
+}
